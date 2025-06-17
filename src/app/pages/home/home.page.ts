e8597e736934bd92301d6fa19af5f2050137ba42
MarianiPedro <pedromariani2211@gmail.com>
2025-06-17 15:29:29 -0300

    150 pokemon

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonThumbnail, IonLabel } from '@ionic/angular/standalone';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonThumbnail, IonLabel,
+    CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
+  pokemons: any[] = [];
+  offset = 0;
+  limit = 150;
 
-  constructor() { }
+  constructor(private pokeapi: PokeapiService) { }
 
   ngOnInit() {
+    this.loadPokemons();
   }
 
+   loadPokemons() {
+    this.pokeapi.getPokemonList(this.offset, this.limit).subscribe(response => {
+      this.pokemons = response.results.map((pokemon: any) => {
+        // Extrai o ID da URL
+        const id = pokemon.url.split('/').filter(Boolean).pop();
+        return {
+          name: pokemon.name,
+          id,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+        };
+      });
+    });
+  }
 }
